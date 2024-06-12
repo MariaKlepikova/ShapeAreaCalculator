4eb31035f991478a8a2db1db2b2299b8c3318ba1
Мария Клепикова <masha.k2001@gmail.com>
2024-06-12 23:09:11 +0300

    [add] AreaCalculator.cs

---
+++ ShapeAreaCalculatorLibrary/AreaCalculator.cs
@@ -0,0 +1,11 @@
+﻿using ShapeAreaCalculatorLibrary.Shapes.Abstract;
+
+namespace ShapeAreaCalculatorLibrary;
+
+public class AreaCalculator
+{
+    public double ExecuteCalculation(ICalculatableAreaOfShape shape)
+    {
+        return shape.CalculateArea();
+    }
+}
